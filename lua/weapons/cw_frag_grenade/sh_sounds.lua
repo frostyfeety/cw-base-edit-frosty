07c0eb372f7e04b2eecf65f3942a510d2d6df014
frosty <dima.isakov03@mail.ru>
2022-05-03 01:26:49 +0800

    initial commit

---
+++ lua/weapons/cw_frag_grenade/sh_sounds.lua
@@ -0,0 +1,4 @@
+CustomizableWeaponry:addReloadSound("CWC_GNADE_PINPULL_START", "weapons/cwc_grenade/pinpull_start.wav")
+CustomizableWeaponry:addFireSound("CWC_GNADE_PINPULL", "weapons/cwc_grenade/pinpull.wav", 1, 70, CHAN_STATIC)
+CustomizableWeaponry:addReloadSound("CWC_GNADE_SPOON", "weapons/cwc_grenade/spoon.wav")
+CustomizableWeaponry:addFireSound("CWC_GNADE_TOSS", {"weapons/cwc_grenade/toss1.wav", "weapons/cwc_grenade/toss2.wav", "weapons/cwc_grenade/toss3.wav"}, 1, 70, CHAN_STATIC)
