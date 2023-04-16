a39ca51e568082c396f862e5afb1a2e4a03a4ca2
frostyfeety <dima.isakov03@mail.ru>
2023-04-16 14:19:08 +0300

    залил новую базу + фикс задержки дыхания

---
+++ lua/cw/shared/colors/cw_partcolors.lua
@@ -0,0 +1,90 @@
+local clr = {}
+clr.name = "part_white"
+clr.display = "White"
+clr.color = Color(255, 255, 255, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
+
+local clr = {}
+clr.name = "part_grey"
+clr.display = "Grey"
+clr.color = Color(90, 90, 90, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
+
+local clr = {}
+clr.name = "part_black"
+clr.display = "Black"
+clr.color = Color(30, 30, 30, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
+
+local clr = {}
+clr.name = "part_tan"
+clr.display = "Tan"
+clr.color = Color(187, 168, 115, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
+
+local clr = {}
+clr.name = "part_brown"
+clr.display = "Brown"
+clr.color = Color(145, 119, 81, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
+
+local clr = {}
+clr.name = "part_green"
+clr.display = "Green"
+clr.color = Color(96, 164, 94, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
+
+local clr = {}
+clr.name = "part_flurogreen"
+clr.display = "Fluorescent Green"
+clr.color = Color(0, 200, 0, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
+
+local clr = {}
+clr.name = "part_blue"
+clr.display = "Blue"
+clr.color = Color(96, 93, 158, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
+
+local clr = {}
+clr.name = "part_fluroblue"
+clr.display = "Fluorescent Blue"
+clr.color = Color(0, 0, 200, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
+
+local clr = {}
+clr.name = "part_yellow"
+clr.display = "Yellow"
+clr.color = Color(215, 232, 74, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
+
+local clr = {}
+clr.name = "part_fluroyellow"
+clr.display = "Fluorescent Yellow"
+clr.color = Color(255, 255, 0, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
+
+local clr = {}
+clr.name = "part_orange"
+clr.display = "Orange"
+clr.color = Color(255, 132, 45, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
+
+local clr = {}
+clr.name = "part_fluroorange"
+clr.display = "Fluorescent Orange"
+clr.color = Color(255, 160, 0, 255)
+
+CustomizableWeaponry.colorableParts:addPartColor(clr)
